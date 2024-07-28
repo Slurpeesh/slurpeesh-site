5eb62ae13a8fcc3fe798a3260a475d8aad82963c
Slurpeesh <JeffNFG@mail.ru>
2024-07-28 15:24:57 +0300

    mobile fix attempt

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -10,7 +10,7 @@ import VkSvg from './svg/VkSvg'
 export default function Footer() {
   const lang = useAppSelector((state) => state.lang.value)
   return (
-    <footer className="flex flex-col justify-center snap-start snap-always h-dvh xs:h-auto">
+    <footer className="flex flex-col justify-center snap-start snap-always h-dvh sm:h-auto">
       <Section
         sectionTitle={getText(lang, 'contacts')}
         id="Contacts"
@@ -42,10 +42,10 @@ export default function Footer() {
             <VkSvg />
           </Link>
         </div>
+        <p className="py-12 text-center font-semibold text-base sm:text-xl">
+          &copy; Slava Shvedko (aka Slurpeesh), {new Date().getFullYear()}
+        </p>
       </Section>
-      <p className="py-12 text-center font-semibold text-base sm:text-xl">
-        &copy; Slava Shvedko (aka Slurpeesh), {new Date().getFullYear()}
-      </p>
     </footer>
   )
 }
